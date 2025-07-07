5a53d7a8f067bab9cd845c61f078b3bc44dddf27
Nicolas Caraballo <nicolas.caraballo@globant.com>
2025-07-07 18:47:18 -0500

    course data

--- modelLoading.py
+++ modelLoading.py
@@ -10,8 +10,12 @@ X_sdt = torch.load('./model/X_sdt.pt', weights_only=True)
 y_mean = torch.load('./model/y_mean.pt', weights_only=True)
 y_std = torch.load('./model/y_std.pt', weights_only=True) 
 
-# Load the model
-model = torch.load('./model/model.pt', weights_only=True)
+# Load the state model
+model = nn.Linear(2,1)  # Assuming a simple linear model with 2 input features
+model.load_state_dict(torch.load('./model/model.pt', weights_only=True))
+
+model.eval()  # Set the model to evaluation mode
+
 
 # create prediction tensor
 
@@ -19,6 +23,10 @@ X_data = torch.tensor([
     [5, 10000],
     [2, 10000],
     [5, 20000],
-                       ], dtype=torch.float32)  # Example: 5 years old car with 20,000 miles
-prediction = model((X_data - X_mean)/ X_sdt)   # Normalize the input data
-print(prediction * y_std + y_mean)  # Denormalize the prediction
+], dtype=torch.float32)  # Example: 5 years old car with 20,000 miles
+
+
+with torch.no_grad():
+    predictions = model((X_data - X_mean) / X_sdt)  # Normalize the input data
+    predictions = predictions * y_std + y_mean  # Denormalize the output data
+
